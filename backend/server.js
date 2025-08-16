036a195adc65ef35690a1401a810a258507cc6dd
Saloni141103 <130493386+Saloni141103@users.noreply.github.com>
2025-08-17 01:14:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,10 +81,10 @@ app.post("/send", async (req, res) => {
 // Serve frontend build
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Start server
